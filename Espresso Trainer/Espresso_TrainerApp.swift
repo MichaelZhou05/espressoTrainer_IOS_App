deb68a3f16b5aed3e2b1faecec4c7dc50e5e7599
Michael Zhou <michael.zhou@duke.edu>
2025-08-06 10:13:58 -0400

    Initial Commit

---
+++ Espresso Trainer/Espresso_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Espresso_TrainerApp.swift
+//  Espresso Trainer
+//
+//  Created by Michael Zhou on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Espresso_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Espresso TrainerTests/Espresso_TrainerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Espresso_TrainerTests.swift
+//  Espresso TrainerTests
+//
+//  Created by Michael Zhou on 8/6/25.
+//
+
+import Testing
+@testable import Espresso_Trainer
+
+struct Espresso_TrainerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
